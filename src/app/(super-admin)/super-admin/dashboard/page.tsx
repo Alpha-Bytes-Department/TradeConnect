4271f3edaf4a3af7e61fbf15765824a87e461024
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-12-18 03:28:07 +0600

    dashboard home page

--- src/app/(super-admin)/super-admin/dashboard/page.tsx
+++ src/app/(super-admin)/super-admin/dashboard/page.tsx
@@ -1,8 +1,8 @@
 // Fahim
 export default function Dashboard() {
     return (
-        <div>
-            <p>dashboard</p>
+        <div className="p-3">
+            <h1 className="font-poppins font- text-4xl">Dashboard Overview</h1>
         </div>
     );
 }
