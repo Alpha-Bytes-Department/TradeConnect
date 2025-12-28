4f7a54d8b2c5dfa667e030fd39b8d30ffbbb7c1f
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2025-12-29 05:00:24 +0600

    settings-completed

--- src/app/(super-admin)/super-admin/settings/page.tsx
+++ src/app/(super-admin)/super-admin/settings/page.tsx
@@ -1,8 +1,12 @@
 // Fahim
+import SettingsPage from "../../components/Settings/SettingsPage";
 export default function Settings() {
     return (
-        <div>
-            settings
+        <div className="p-6 bg-[#F6F6F6]">
+            <h1 className="font-poppins font-medium text-[#0B0B0B] text-2xl">Settings</h1>
+            <p className="font-poppins text-[#626262] text-sm">Manage system settings and
+                configuration</p>
+            <SettingsPage />
         </div>
     );
 }
