af578606a14dbe0217f82d779a36ca50c356f914
Saikh Seraj Fahim <saikhseraj2017@gmail.com>
2026-01-20 08:28:55 +0600

    Settings-API-Integrated

--- src/app/(super-admin)/components/allBusiness/GridView.tsx
+++ src/app/(super-admin)/components/allBusiness/GridView.tsx
@@ -35,12 +35,12 @@ export default function GridView({ total, currentPage, onPageChange }: GridViewP
                 <div>
                     <div className="grid grid-cols-1 xl:grid-cols-3 2xl:grid-cols-4 gap-8 mt-8">
                         {businesses.map(item => (
-                            <div key={item.id} className="h-[460px] rounded-lg border bg-[#FFFFFF] 
+                            <div key={item?.id} className="h-[460px] rounded-lg border bg-[#FFFFFF] 
                             shadow-lg">
                                 <div className="relative h-1/3">
-                                    {item.logo ? (
+                                    {item?.logo ? (
                                         <Image
-                                            src={item.logo}
+                                            src={item?.logo}
                                             alt="Business logo"
                                             fill
                                             className="object-cover object-center rounded-t-lg"
@@ -48,7 +48,7 @@ export default function GridView({ total, currentPage, onPageChange }: GridViewP
                                     ) : (
                                         <div className="w-full h-full bg-gray-200 rounded-t-lg" />
                                     )}
-                                    {item.is_locked === false ? (<p className="absolute top-4 right-4 
+                                    {item?.is_locked === false ? (<p className="absolute top-4 right-4 
                                 inline-block px-2 py-1 bg-[#BAFFB4] text-[#1A6300] border-[#279300]  
                                 rounded-full font-poppins text-sm">Active</p>)
                                         : (<p className="absolute top-4 right-4 
@@ -60,38 +60,41 @@ export default function GridView({ total, currentPage, onPageChange }: GridViewP
                                     <div className="flex gap-3">
                                         <div className="">
                                             <h1 className="font-poppins font-semibold text-[#434343]">
-                                                {item.business_name}</h1>
+                                                {item?.business_name}</h1>
                                         </div>
-                                        {/* <p>{getFlagEmoji(item.flag_src)}</p> */}
+                                        {item?.country?.flag &&
+                                            <Image src={item?.country?.flag} alt={item?.country?.name}
+                                                width={24} height={24} />
+                                        }
                                     </div>
                                     <div className="flex items-center gap-1">
                                         <MapPin className="text-[#909090] w-4 h-4" />
                                         <p className="font-poppins text-[#909090] text-sm">
-                                            {item.country_name}</p>
+                                            {item?.full_address}{", "}{item?.country?.name}</p>
                                     </div>
-                                    <div className="mt-4">
+                                    <div className="mt-2">
                                         <p className="font-poppins text-[#3F3F3F] text-sm">
-                                            {item.about_business}</p>
+                                            {item?.about_business}</p>
                                     </div>
 
-                                    <p className="font-poppins text-[#595959] text-xs mt-4">Services:</p>
+                                    <p className="font-poppins text-[#595959] text-sm mt-2">Services:</p>
                                     <div className="flex flex-wrap gap-2 mt-1">
-                                        {item.services.slice(0, 2).map(
+                                        {item?.services?.slice(0, 2).map(
                                             (service: { id: string; title: string }) => (
                                                 <p
-                                                    key={service.id}
+                                                    key={service?.id}
                                                     className="bg-[#BFD7FD] inline-block px-2 py-1 
                                                     font-poppins text-[#153569] text-sm rounded-full"
                                                 >
-                                                    {service.title}
+                                                    {service?.title}
                                                 </p>
                                             )
                                         )}
 
-                                        {item.services.length > 2 && (
+                                        {item?.services?.length > 2 && (
                                             <p className="bg-[#BFD7FD] inline-block px-2 py-1 font-poppins
                                             text-[#153569] text-sm rounded-full">
-                                                +{item.services.length - 2}
+                                                +{item?.services?.length - 2}
                                             </p>
                                         )}
                                     </div>
@@ -101,15 +104,15 @@ export default function GridView({ total, currentPage, onPageChange }: GridViewP
                                             <button className="w-full py-2 bg-[#BFD7FD] font-poppins 
                                         font-medium text-[#2459B1] text-center rounded-lg cursor-pointer"
                                                 onClick={() =>
-                                                    router.push(`/super-admin/business-details/${item.id}`)}>
+                                                    router.push(`/super-admin/business-details/${item?.id}`)}>
                                                 View Profile</button>
                                         </div>
-                                        <div className="">
+                                        <div className="col-span-1">
                                             <button className="w-full py-2 bg-[#FFFFFF] font-poppins 
                                         font-medium text-[#2459B1] text-center rounded-lg cursor-pointer 
                                         border border-[#1C4589]"
                                                 onClick={() =>
-                                                    router.push(`/super-admin/edit-business/${item.id}`)}>
+                                                    router.push(`/super-admin/edit-business/${item?.id}`)}>
                                                 Edit</button>
                                         </div>
                                     </div>
