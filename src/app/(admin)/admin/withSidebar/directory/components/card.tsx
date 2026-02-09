4ceaa6d6f2690bc81f5a4270a4eb53254363a9ed
mm <mmislam272@gmail.com>
2026-02-10 00:45:24 +0600

    featured 3

--- src/app/(admin)/admin/withSidebar/directory/components/card.tsx
+++ src/app/(admin)/admin/withSidebar/directory/components/card.tsx
@@ -72,6 +72,12 @@ const Card: React.FC<CardProps> = ({ prop }) => {
         <div className={`flex flex-col h-full col-span-4 md:col-span-1 w-full ${is_featured ?'bg-[#FFF5E9]':'bg-white'} rounded-2xl shadow-lg overflow-hidden border `}>
             {/* Header Image */}
             <div className="relative w-full h-[143px]">
+                <div className="fc relative h-8 w-8 z-99 left-5 top-5">
+                    {is_featured && (
+                        <Image src={"/Frame.svg"} alt="badge" height={36} width={36} />
+
+                    )}
+                </div>
                 <Image
                     src={headerImage ? headerImage : "/image.png"}
                     alt="Header"
@@ -87,17 +93,12 @@ const Card: React.FC<CardProps> = ({ prop }) => {
                     
                     <div className="max-w-full flex-1 flex flex-col">
                         <div className=" max-w-full flex flex-row items-center gap-2">
-                            <button className=" max-w-[calc(100%-50px)] " onClick={async () => { await increaseBusinessView(id); router.push(`/admin/withoutSidebar/accounts/${id}/`) }}>
+                            <button className=" max-w-[calc(100%-30px)] " onClick={async () => { await increaseBusinessView(id); router.push(`/admin/withoutSidebar/accounts/${id}/`) }}>
                                 <h2 className="text-lg md:text-xl font-semibold text-gray-800 mb-1 truncate hover:text-blue-800">
                                 {title}
                                 </h2>
                             </button>
-                            <div className="fc max-h-6 max-w-6">
-                                {is_featured && (
-                                <Image src={"/Featured-Business-Badge.svg"} alt="badge" height={16} width={16} />
-
-                            )}
-                            </div>
+                            
                             <Flag id={country} h={16} w={16}/>
                         </div>
                         
